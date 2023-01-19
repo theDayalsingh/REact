23b2ee094adb5672bf3efe7fbb44d8bb79c6d61b
thedayalsingh <thedayalsingh@gmail.com>
2023-01-19 20:46:38 +0530

    expensseComponent

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>Expense Items
+        <div>Food Rs 10</div>
+        <div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+        </div>
+        </div>
+    )
+	
+}
+export default ExpenseItem
