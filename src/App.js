d89d4ab04f6ad71b68a2912507011ca3302f2ca4
theDayalsingh <thedayalsingh@gmail.com>
2023-01-19 22:21:14 +0530

    Addloop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import ExpenseItem from "./Components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      loe: 'LocationOfExpenditure 1',
+      title: 'Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { loe: 'LocationOfExpenditure 2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      loe: 'LocationOfExpenditure 3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      loe: 'LocationOfExpenditure 4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  let expensesList=expenses.map((item,index)=>{
+    return <ExpenseItem date={expenses[index].date} title={expenses[index].title} amount={expenses[index].amount} loe={expenses[index].loe}></ExpenseItem>
+  })
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+      {expensesList}
+       </div>
   );
 }
 
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,12 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+function ExpenseItem(props){
+const LocationOfExpenditure ="LocationOfExpenditure"
     return (
-        <div>Expense Items
-        <div>Food Rs 10</div>
-        <div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+        <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+        <h2>{props.title} {props.loe}</h2>
+        <div className="expense-item__price">${props.amount}</div>
         </div>
         </div>
     )
