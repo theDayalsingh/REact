a6f6d427d06e2f9e93abebd08894319240c24773
theDayalsingh <thedayalsingh@gmail.com>
2023-01-19 23:10:01 +0530

    expensedetails

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  let expensesList=expenses.map((item,index)=>{
-    return <ExpenseItem date={expenses[index].date} title={expenses[index].title} amount={expenses[index].amount} loe={expenses[index].loe}></ExpenseItem>
-  })
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expensesList}
-       </div>
+      <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} loe={expenses[0].loe}></ExpenseItem>
+      <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} loe={expenses[1].loe}></ExpenseItem>
+      <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} loe={expenses[2].loe}></ExpenseItem>
+      <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount} loe={expenses[3].loe}></ExpenseItem>
+    </div>
   );
 }
 
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,13 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props){
-const LocationOfExpenditure ="LocationOfExpenditure"
     return (
         <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
-        <div className="expense-item__description">
-        <h2>{props.title} {props.loe}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        </div>
+        <ExpenseDate date={props.date} />
+        <ExpenseDetails title={props.title} amount={props.amount} />
         </div>
     )
 	
