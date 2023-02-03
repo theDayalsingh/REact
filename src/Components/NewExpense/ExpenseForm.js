f7fee47ac02a36534221b4285f66905770d1bac2
theDayalsingh <thedayalsingh@gmail.com>
2023-02-04 00:58:37 +0530

    Add Chart

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -43,10 +43,10 @@ const ExpenseForm = (props) => {
     event.preventDefault();
 
     const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
-    };
+        title: enteredTitle,
+        amount: +enteredAmount,
+        date: new Date(enteredDate),
+      };
 
     props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
@@ -87,6 +87,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+      <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
